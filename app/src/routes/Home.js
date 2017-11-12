01c85a455d62b38ad3bf0d871b5874280aaf3212
Muhammad Rehman <muh316@gmail.com>
2017-11-12 16:20:20 -0500

    Attempt to add profile data to home

--- app/src/routes/Home.js
+++ app/src/routes/Home.js
@@ -1,5 +1,36 @@
 import React from 'react';
 
-export default () => (
-	<div> home</div>
-);
+class Home extends React.Component{
+
+	state = {
+		//userId: '',
+		firstName: '',
+		lastName: '',
+		email: '',
+	}
+
+	componentDidMount(props){
+		fetch('/api/home')
+		.then(data => data.json())
+		.then(data => {
+			console.log(data);	
+		}) 
+	
+	}
+
+	
+
+	render(){
+		return(
+	<div> <h1>Welcome home</h1>
+	<p> Your name is TestName;</p>
+	
+
+	</div>
+	);
+	}
+
+
+}
+
+export default Home;
