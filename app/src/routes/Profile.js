413cd3a4778966ac39e5f5284365c571396727d3
Muhammad Rehman <muh316@gmail.com>
2017-11-12 15:41:09 -0500

    Added profile page rendering from react

--- app/src/routes/Profile.js
+++ app/src/routes/Profile.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+
 
 class Profile extends React.Component{
-
 
 	state = {
 			firstName: '',
 			lastName: '',
 			email: '',
+			id: this.props.match.params.id,
 		}
 
-	componentWillMount(props){
-		fetch('/api/profile')
-		.then()=> 
+	componentDidMount(props){
+		fetch('/api/profile/' + this.state.id)
+		.then(d => d.json())
+		.then(d => {
+			this.setState({
+				firstName: d.firstName,
+				lastName: d.lastName,
+				email: d.email,
+			})
+			
+		}) 
 	}
 
 
-	render(){
+
+	render(props){
 		return(
 			<div>
-			"testApp"
+			<h1> This Profile Belongs to {this.state.firstName}</h1>
+			<p>
+			Your email is {this.state.email} <br />
+			Your name is {this.state.firstName} {this.state.lastName} <br />
+			The id here is {this.state.id}
+			</p>
+			<p> Click <Link to="/home">Here</Link> to go home</p>
 			</div>);
 	}
 }
--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const Users = require('../models').Users;
+
 
 router.get('/', (req, res) => {
-  res.render('home')
+	res.json(JSON.stringify(req.user))
+
+
 });
+
+
 
 module.exports = router;
