3b1f037af1c25d9b5f79744e8c0ebbb281f0d1b3
jwong28 <jeffersonwong28@gmail.com>
2017-11-14 19:45:23 -0500

    Minor changes before merge

--- app/src/routes/Login.js
+++ app/src/routes/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -44,6 +45,13 @@ class Login extends React.Component{
 				password: '',
 			})
 		};
+		axios.post('/api/Login',
+			{
+				
+			})
+		.then((result => {
+					console.log(result)
+				}))
 
 	}
 
@@ -74,6 +82,8 @@ class Login extends React.Component{
 				<br />
 
 				<RaisedButton label = "Submit" onClick = {e => this.onSubmit(e)} primary />
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<RaisedButton label = "SignUp" onClick = {e => this.onSignup(e)} primary />
 			</form>
 			</MuiThemeProvider>
 			);
